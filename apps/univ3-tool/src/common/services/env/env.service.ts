f7f1b51dc6fd438094c4bab2ede665514062b6ce
SG00536ML <sg00536ml@SG00536MLs-MacBook-Pro.local>
2022-08-25 10:54:02 +0800

    init

--- apps/univ3-tool/src/common/services/env/env.service.ts
+++ apps/univ3-tool/src/common/services/env/env.service.ts
@@ -22,14 +22,7 @@ export class EnvService {
   }
   get endPoint() {
     const envs = {
-      WSS_DOMAIN: process.env.WSS_DOMAIN,
-      API2_DOMAIN: process.env.API2_DOMAIN,
-      ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
-      ALGOLIA_COIN_API_KEY: process.env.ALGOLIA_COIN_API_KEY,
-      ALGOLIA_COIN_INDEX_NAME: process.env.ALGOLIA_COIN_INDEX_NAME,
-      ALARM_APP_ID: process.env.ALARM_APP_ID,
-      ALARM_SECRET: process.env.ALARM_SECRET,
-      ALARM_LINK: process.env.ALARM_LINK,
+      
     };
     return envs;
   }
--- apps/univ3-tool/src/main.ts
+++ apps/univ3-tool/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+const isDev = process.env.ENV === 'dev';
+const isProd = process.env.ENV === 'prod';
+
+function initEnvVars() {
+  const envPath = isDev ? './env/dev.conf' : './.env';
+  const ENV = dotenv.config({
+    path: path.resolve(process.cwd(), envPath),
+  });
+  if (!isProd) {
+    console.log(ENV.parsed);
+  }
+}
+initEnvVars();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8901);
 }
 bootstrap();
