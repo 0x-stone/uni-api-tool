42b46ebe7e682d266ecfcc7d9b826f7ea67a7fba
SG00536ML <stone.liu@bybit.com>
2022-08-29 14:40:58 +0800

    init

--- apps/univ3-tool/src/pool/pool.service.spec.ts
+++ apps/univ3-tool/src/pool/pool.service.spec.ts
@@ -1,19 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { EnvService } from '../common/services/env/env.service';
 import { PoolService } from './pool.service';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 
 describe('PoolService', () => {
   let service: PoolService;
 
   beforeEach(async () => {
+    function initEnvVars() {
+      const envPath = './env/test.conf';
+      const ENV = dotenv.config({
+        path: path.resolve(process.cwd(), envPath),
+      });
+    }
+    initEnvVars();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PoolService],
+      providers: [PoolService, EnvService],
     }).compile();
 
     service = module.get<PoolService>(PoolService);
-
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('mint encode should be correct', () => {
+    expect(
+      service.encodeMint({
+        token0: '0x6070733C068a5a30054A751605eFdC3a22e02d64',
+        token1: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+        fee: '100',
+        tickLower: '-115136',
+        tickUpper: '-92109',
+        amount0Desired: '49882979211620961850079940376',
+        amount1Desired: '0',
+        amount0Min: '49701278734004365626583707103',
+        amount1Min: '0',
+        recipient: '0xCB4B715c154b47CA51be4864f979e217912a4FF7',
+        deadline: '1653541190',
+      }),
+    ).toBeTruthy();
   });
 });
--- apps/univ3-tool/src/pool/pool.service.ts
+++ apps/univ3-tool/src/pool/pool.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@nestjs/common';
-import { Contract, ethers } from 'ethers';
-import { Pool } from '@uniswap/v3-sdk';
-import { Token } from '@uniswap/sdk-core';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { BigNumber, Contract, ethers } from 'ethers';
 import { abi as INonfungiblePositionManager } from '@uniswap/v3-periphery/artifacts/contracts/interfaces/INonfungiblePositionManager.sol/INonfungiblePositionManager.json';
+import { abi as IPoolFactory } from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Factory.sol/IUniswapV3Factory.json';
+import { abi as IPool } from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json';
 import { EnvService } from '../common/services/env/env.service';
 
 interface ImintParams {
   token0: string;
   token1: string;
-  fee: string;
+  fee: number;
   tickLower: string;
   tickUpper: string;
   amount0Desired: string;
@@ -19,40 +19,104 @@ interface ImintParams {
   deadline: string;
 }
 
+
 @Injectable()
 export class PoolService {
-  private _poolContract: Contract = this._createPoolConstract();
+  private _provider: ethers.providers.JsonRpcProvider =
+    new ethers.providers.JsonRpcProvider(this.envService.endPoint.NODE_LINK);
 
-  constructor(private envService: EnvService) {
-    console.log(this._encodeMint());
-  }
+  private _poolManage: Contract = this._createManageConstract();
 
-  private _createPoolConstract() {
-    const provider = new ethers.providers.JsonRpcProvider(
-      this.envService.endPoint.NODE_LINK,
-    );
+  private _poolFactory: Contract = this._createFactoryConstract();
+
+  constructor(private envService: EnvService) {}
+
+  private MIN_TICK = -887272;
+
+  private MAX_TICK = -this.MIN_TICK;
+
+  private _createManageConstract() {
     return new ethers.Contract(
       this.envService.endPoint.UNI_V3_POOL_MANAGE,
       INonfungiblePositionManager,
-      provider,
+      this._provider,
     );
   }
 
-  private _encodeMint() {
-    return this._poolContract.interface.encodeFunctionData('mint', [
-      {
-        token0: '0x6070733C068a5a30054A751605eFdC3a22e02d64',
-        token1: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-        fee: '100',
-        tickLower: '-115136',
-        tickUpper: '-92109',
-        amount0Desired: '49882979211620961850079940376',
-        amount1Desired: '0',
-        amount0Min: '49701278734004365626583707103',
-        amount1Min: '0',
-        recipient: '0xCB4B715c154b47CA51be4864f979e217912a4FF7',
-        deadline: '1653541190',
-      },
-    ]);
+  private _createFactoryConstract() {
+    return new ethers.Contract(
+      this.envService.endPoint.UNI_V3_FACTORY,
+      IPoolFactory,
+      this._provider,
+    );
+  }
+
+  encodeMint(params: ImintParams) {
+    return this._poolManage.interface.encodeFunctionData('mint', [params]);
+  }
+
+  async getPoolContract(
+    token1: string,
+    token2: string,
+    fee: number,
+  ): Promise<Contract> {
+    const contractAddress = await this._poolFactory.getPool(
+      token1,
+      token2,
+      fee,
+    );
+    return new ethers.Contract(contractAddress, IPool, this._provider);
+  }
+
+  getTickSpacing(poolContract: Contract) {
+    return poolContract.tickSpacing();
+  }
+
+  async checkRange(
+    tickLower: number,
+    tickUpper: number,
+    token0: string,
+    token1: string,
+    fee: number,
+  ) {
+    if (tickLower < tickUpper) {
+      throw new HttpException(
+        'tickLower should < tickUpper',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    if (tickLower < this.MIN_TICK) {
+      throw new HttpException('tickLower too low', HttpStatus.BAD_REQUEST);
+    }
+    if (tickUpper > this.MAX_TICK) {
+      throw new HttpException('tickUpper too high', HttpStatus.BAD_REQUEST);
+    }
+
+    const tickSpacing = await this.getTickSpacing(
+      await this.getPoolContract(token0, token1, fee),
+    );
+    if (tickLower % tickSpacing !== 0) {
+      throw new HttpException(
+        'tickLower % tickSpacing',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    if (tickUpper % tickSpacing !== 0) {
+      throw new HttpException(
+        'tickLower % tickSpacing',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async mint(params: ImintParams) {
+    const { tickLower, tickUpper, token0, token1, fee } = params;
+    await this.checkRange(
+      BigNumber.from(tickLower).toNumber(),
+      BigNumber.from(tickUpper).toNumber(),
+      token0,
+      token1,
+      fee,
+    );
   }
 }
