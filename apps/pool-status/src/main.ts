469fb444218e76970d6c434f39caab2795e58d5e
SG00536ML <stone.liu@bybit.com>
2022-09-08 16:47:49 +0800

    chore: rm useless

--- apps/pool-status/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { PoolStatusModule } from './pool-status.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(PoolStatusModule);
-  await app.listen(3000);
-}
-bootstrap();
